27c90060a1bc2b18beefd1a97315eda86c0e1094
ff-dcamell <dcamell@firstfuel.com>
2019-05-23 14:01:54 -0400

    how will this affect it??

--- test_inner/inner_vision.py
+++ test_inner/inner_vision.py
@@ -1,4 +1,5 @@
 #first, let's edit master branch under source directory ~/Documents/git/test_inner/inner_vision.py
+#NOW... let's edit from the subrepo version and try to push back upstream...
 
 def tfunc(input_val):
     return f"You said '{input_val}'?"
